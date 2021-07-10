e355200b5229e50a6b637cd2372dbd8845d35c70
Ajith Ramachandran <ajithar204@gmail.com>
2021-07-10 12:47:07 +0530

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import NavbarComponent from '../components/Navbar'
+
+const Home = () => {
+    return (
+        <>
+            < NavbarComponent />
+            <div className="container">
+                <h3>This project is currently under development.</h3>
+            </div>
+        </>
+    )
+}
+
+export default Home
